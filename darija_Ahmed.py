a8654da8b575bb6a48347f38e08a8a49fabeb13a
AhmedELkhdadi <52387180+AhmedELkhdadi@users.noreply.github.com>
2021-01-17 20:29:29 +0000

    if else, while, conditions

--- darija_Ahmed.py
+++ darija_Ahmed.py
@@ -18,12 +18,19 @@ tokens = [
     'DIVIDE',
     'LPAREN',
     'RPAREN',
+    'LBRACKET',
+    'RBRACKET',
     'ID',
     'STRING',
     'EQUALS',
     'INCREMENTATION',
     'DECREMENTATION',
     'SEMICOLON',
+    'SUP',
+    'INF',
+    'EQUALSCOMP',
+    'INFEQUALS',
+    'SUPEQUALS',
 
 ]
 
@@ -70,6 +77,11 @@ reserved = {
 tokens = tokens + list(reserved.values())
 
 # Regular expression rules for simple tokens
+t_EQUALSCOMP = r'\=\='
+t_SUP = r'\>'
+t_INF = r'\<'
+t_INFEQUALS = r'\<\='
+t_SUPEQUALS = r'\>\='
 t_INCREMENTATION = r'\+\+'
 t_DECREMENTATION = r'--'
 t_PLUS = r'\+'
@@ -80,8 +92,10 @@ t_EQUALS = r'\='
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_SEMICOLON = r';'
+t_LBRACKET = r'\{'
+t_RBRACKET = r'\}'
 
-# literals = [ '+','-','*','/' ]
+# literals = ['{', '}', '==']
 
 
 def t_COMMENT(t):
@@ -103,6 +117,24 @@ def t_STRING(t):
 def t_FLOAT(t):
     r'\d+\.\d+'
     t.value = float(t.value)
+    return t
+
+
+def t_S7I7(t):
+    r's7i7'
+    t.value = True
+    return t
+
+
+def t_KHATE2(t):
+    r'khate2'
+    t.value = False
+    return t
+
+
+def t_WALO(t):
+    r'walo'
+    t.value = None
     return t
 
 
@@ -147,6 +179,9 @@ lexer = lex.lex()
 precedence = (
     ('left', 'PLUS', 'MINUS'),
     ('left', 'TIMES', 'DIVIDE'),
+    ('left', 'WA', 'AW'),
+    ('nonassoc', 'SUP', 'INF', 'SUPEQUALS', 'INFEQUALS', 'EQUALSCOMP'),
+    ('nonassoc', 'LPAREN', 'RPAREN')
 
 )
 
@@ -158,6 +193,8 @@ def p_darija(p):
            | incrementation
            | decrementation
            | expression
+           | if
+           | while
            | empty
     '''
     run(p[1])
@@ -186,18 +223,109 @@ def p_var_assign(p):
     p[0] = ('=', p[1], p[3])
 
 
-# def p_lkola(p):
+# def p_ifElse(p):
 #     '''
-#
+#     if_else : ILA LPAREN condition RPAREN LBRACKET block RBRACKET WLA LBRACKET block RBRACKET
 
 #     '''
+#     p[0] = (p[1], p[3], p[6])
+
+
+def p_if(p):
+    '''
+    if : ILA LPAREN condition RPAREN LBRACKET block RBRACKET 
+        | ILA LPAREN condition RPAREN LBRACKET block RBRACKET WLA LBRACKET block RBRACKET
+
+    '''
+    if len(p) == 8:
+        p[0] = (p[1], p[3], p[6])
+    else:
+        p[0] = (p[1], p[3], p[6], p[10])
+
+
+def p_while(p):
+    '''
+    while : MA7ED LPAREN condition RPAREN LBRACKET block RBRACKET
+
+    '''
+    p[0] = (p[1], p[3], p[6])
+
+
+def p_block(p):  # block to be executed when condition is satisfied,
+    # didnt do "darija" function because darija initiates run function, thus it will execute even if condition is not satisfied
+    '''
+    block : var_assign
+           | printing
+           | incrementation
+           | decrementation
+           | expression
+           | if
+           | while
+           | empty
+    '''
+    p[0] = p[1]
+
+
+def p_condition_big(p):
+    '''
+    condition : LPAREN condition RPAREN AW LPAREN condition RPAREN
+              | LPAREN condition RPAREN WA LPAREN condition RPAREN
+
+    '''
+    p[0] = (p[4], p[2], p[6])
+
+
+def p_condition_medium1(p):
+    '''
+    condition : condition WA LPAREN condition RPAREN
+              | condition AW LPAREN condition RPAREN
+
+    '''
+    p[0] = (p[2], p[1], p[4])
+
+
+def p_condition_medium2(p):
+    '''
+    condition : LPAREN condition RPAREN WA condition
+              | LPAREN condition RPAREN AW condition
+
+    '''
+    p[0] = (p[4], p[2], p[5])
+
+
+def p_condition(p):
+    '''
+    condition :  condition WA condition
+              |  condition AW condition
+
+    '''
+    p[0] = (p[2], p[1], p[3])
+
+
+def p_condition_comp(p):
+    '''
+    condition : expression SUP expression
+              | expression INF expression
+              | expression EQUALSCOMP expression
+              | expression SUPEQUALS expression
+              | expression INFEQUALS expression
+    '''
+    p[0] = (p[2], p[1], p[3])
+
+
+def p_condition_exp(p):
+    '''
+    condition : expression
+    '''
+    p[0] = p[1]
+
 
 def p_expression(p):
     '''
     expression : expression PLUS expression
-              | expression MINUS expression
-              | expression TIMES expression
-              | expression DIVIDE expression
+               | expression MINUS expression
+               | expression TIMES expression
+               | expression DIVIDE expression
     '''
     p[0] = (p[2], p[1], p[3])
 
@@ -209,11 +337,14 @@ def p_expression_id(p):
     p[0] = ('id', p[1])
 
 
-def p_expression_int_float_string(p):
+def p_expression_terminals(p):
     '''
     expression : INT
                | FLOAT
                | STRING
+               | KHATE2
+               | S7I7
+               | WALO
     '''
     p[0] = p[1]
 
@@ -223,8 +354,9 @@ def p_printing(p):
     printing : KTEB LPAREN expression RPAREN
             | KTEB LPAREN incrementation RPAREN
             | KTEB LPAREN decrementation RPAREN
+            | KTEB LPAREN condition RPAREN
     '''
-    print(run(p[3]))
+    p[0] = (p[1], p[3])
 
 
 def p_empty(p):
@@ -234,11 +366,15 @@ def p_empty(p):
     p[0] = None
 
 
-env = {}
+def p_error(p):
+    print("Syntax error at ", p.value)
+
+
+ids = {}
 
 
 def run(p):
-    global env
+    global ids
     if type(p) == tuple:
         if p[0] == '+':
             return run(p[1]) + run(p[2])
@@ -249,15 +385,41 @@ def run(p):
         elif p[0] == '/':
             return run(p[1]) / run(p[2])
         elif p[0] == '++':
-            env[p[1]] = env[p[1]] + 1
-            return env[p[1]]
+            ids[p[1]] = ids[p[1]] + 1
+            return ids[p[1]]
         elif p[0] == '--':
-            env[p[1]] = env[p[1]] - 1
-            return env[p[1]]
+            ids[p[1]] = ids[p[1]] - 1
+            return ids[p[1]]
         elif p[0] == '=':
-            env[p[1]] = run(p[2])
+            ids[p[1]] = run(p[2])
         elif p[0] == 'id':
-            return env[p[1]]
+            return ids[p[1]]
+        elif p[0] == 'kteb':
+            print(run(p[1]))
+        elif p[0] == 'wa':
+            return run(p[1]) and run(p[2])
+        elif p[0] == 'aw':
+            return run(p[1]) or run(p[2])
+        elif p[0] == '==':
+            return run(p[1]) == run(p[2])
+        elif p[0] == '>=':
+            return run(p[1]) >= run(p[2])
+        elif p[0] == '<=':
+            return run(p[1]) <= run(p[2])
+        elif p[0] == '>':
+            return run(p[1]) > run(p[2])
+        elif p[0] == '<':
+            return run(p[1]) < run(p[2])
+        elif p[0] == "ila":
+            if run(p[1]):
+                run(p[2])
+            else:
+                if len(p) > 3:
+                    run(p[3])
+
+        elif p[0] == "ma7ed":
+            while run(p[1]):
+                run(p[2])
     else:
         return p
 
